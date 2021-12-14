e315f6eca0377d2862d20402f727f0c4800121c5
FelipeTriana <felipe.trianag@udea.edu.co>
2021-12-13 22:05:45 -0500

    First commit

---
+++ src/main/java/com/udea/biosegura/BioseguraApplication.java
@@ -0,0 +1,13 @@
+package com.udea.biosegura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioseguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioseguraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/biosegura/BioseguraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.biosegura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioseguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
