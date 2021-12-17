88f5b3e02b32ae8bf0551fb6eb7b47a81ba5bcd5
FelipeTriana <felipe.trianag@udea.edu.co>
2021-12-17 01:17:56 -0500

    ts

---
+++ src/main/java/com/udea/biosegura/persistence/crud/UserCrudRepository.java
@@ -0,0 +1,8 @@
+package com.udea.biosegura.persistence.crud;
+
+import com.udea.biosegura.persistence.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserCrudRepository extends CrudRepository<User, String> {
+
+}
