c7d4cf0d4642d94134d6043f87f69e71e2887d15
Felipe Triana Gomez <ftrianag@sura.com.co>
2022-01-09 13:44:39 -0500

    fixed InvitationCrudRepository(primary key type)

--- src/main/java/com/udea/biosegura/persistence/crud/InvitationCrudRepository.java
+++ src/main/java/com/udea/biosegura/persistence/crud/InvitationCrudRepository.java
@@ -1,7 +1,8 @@
 package com.udea.biosegura.persistence.crud;
 
 import com.udea.biosegura.persistence.entity.Invitation;
+import com.udea.biosegura.persistence.entity.InvitationPK;
 import org.springframework.data.repository.CrudRepository;
 
-public interface InvitationCrudRepository extends CrudRepository<Invitation, String> {
+public interface InvitationCrudRepository extends CrudRepository<Invitation, InvitationPK> {
 }
