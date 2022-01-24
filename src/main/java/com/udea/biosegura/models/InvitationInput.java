cc550d866a86f0b090c92350979bdadcbe2e5743
mBaenaCh <mateo.baena@hotmail.com>
2022-01-24 17:33:45 -0500

    Feature: Invitation input class

---
+++ src/main/java/com/udea/biosegura/models/InvitationInput.java
@@ -0,0 +1,9 @@
+package com.udea.biosegura.models;
+
+public class InvitationInput {
+    private String userId;
+    private String placeId;
+    private String inDate;
+    private String outDate;
+
+}
